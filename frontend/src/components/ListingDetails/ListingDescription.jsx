0ec167387251256fd77f91ae7d5ed9f7121969a6
vivekkr620 <gk6629086@gmail.com>
2026-07-20 22:15:15 +0530

    feat: Improving Code

--- frontend/src/components/ListingDetails/ListingDescription.jsx
+++ frontend/src/components/ListingDetails/ListingDescription.jsx
@@ -1,15 +1,3 @@
-// export default function ListingDescription({ listing }) {
-//   return (
-//     <main className="max-w-7xl mx-auto px-6 py-8">
-//       <h2 className="text-2xl font-semibold mb-4">About this place</h2>
-
-//       <p className="text-gray-600 leading-7">{listing.description}</p>
-//     </main>
-//   );
-// }
-
-
-
 export default function ListingDescription({ listing }) {
   return (
     <div>
--- frontend/src/components/ListingDetails/ReviewSummary.jsx
+++
@@ -1,3 +0,0 @@
-export default function ReviewSummary() {
-    return null;
-}

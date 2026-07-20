5a8e0f25e32ef03f0609b4be34ddb2da2cb38d46
vivekkr620 <gk6629086@gmail.com>
2026-07-20 22:13:34 +0530

    feat: Add ReviewSection

--- frontend/src/pages/ListingDetailsPage.jsx
+++ frontend/src/pages/ListingDetailsPage.jsx
@@ -1,13 +1,18 @@
 import { useEffect, useState, useContext } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
+import { useCallback } from "react";
+
 import api from "../services/api";
 import AuthContext from "../context/AuthContext";
 
 import ListingHero from "../components/ListingDetails/ListingHero";
 import ListingInfo from "../components/ListingDetails/ListingInfo";
 import ListingDescription from "../components/ListingDetails/ListingDescription";
+
 import BookingCard from "../components/ListingDetails/BookingCard";
+
+import ReviewSection from "../components/Reviews/ReviewSection";
 
 export default function ListingDetailsPage() {
   const { id } = useParams();
@@ -18,11 +23,28 @@ export default function ListingDetailsPage() {
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
 
+  const fetchListing = useCallback(async () => {
+    const res = await api.get(`/listings/${id}`);
+
+    console.log("Listing Response: ", res.data)
+
+    return res.data.listing;
+  }, [id]);
+
+  const refreshListing = async () => {
+    try {
+      const data = await fetchListing();
+      setListing(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   useEffect(() => {
-    const fetchListing = async () => {
+    const loadListing = async () => {
       try {
-        const res = await api.get(`/listings/${id}`);
-        setListing(res.data.listing);
+        const data = await fetchListing();
+        setListing(data);
       } catch (err) {
         console.error(err);
       } finally {
@@ -30,12 +52,12 @@ export default function ListingDetailsPage() {
       }
     };
 
-    fetchListing();
-  }, [id]);
+    loadListing();
+  }, [fetchListing]);
 
   const handleDelete = async () => {
     const confirmDelete = window.confirm(
-      "Are you sure you want to delete this listing?"
+      "Are you sure you want to delete this listing?",
     );
 
     if (!confirmDelete) return;
@@ -55,19 +77,12 @@ export default function ListingDetailsPage() {
     } catch (err) {
       console.error(err);
 
-      alert(
-        err.response?.data?.message ||
-          "Unable to delete listing"
-      );
+      alert(err.response?.data?.message || "Unable to delete listing");
     }
   };
 
   if (loading) {
-    return (
-      <div className="text-center py-20 text-xl">
-        Loading Listing...
-      </div>
-    );
+    return <div className="text-center py-20 text-xl">Loading Listing...</div>;
   }
 
   if (!listing) {
@@ -79,28 +94,22 @@ export default function ListingDetailsPage() {
   }
 
   const isOwner =
-    isAuthenticated &&
-    user &&
-    listing.owner &&
-    user.id === listing.owner._id;
-
+    isAuthenticated && user && listing.owner && user.id === listing.owner._id;
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 mb-20">
       <ListingHero listing={listing} />
 
+      {/* Listing + Booking */}
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-12 mt-10">
+        {/* Left Side */}
         <div className="lg:col-span-2 flex flex-col gap-8">
-
           <ListingInfo listing={listing} />
 
           {/* Owner Actions */}
           {isOwner && (
             <div className="flex gap-4">
-
               <button
-                onClick={() =>
-                  navigate(`/listings/${listing._id}/edit`)
-                }
+                onClick={() => navigate(`/listings/${listing._id}/edit`)}
                 className="bg-yellow-500 hover:bg-yellow-600 text-white px-6 py-3 rounded-lg font-medium transition"
               >
                 Edit Listing
@@ -112,7 +121,6 @@ export default function ListingDetailsPage() {
               >
                 Delete Listing
               </button>
-
             </div>
           )}
 
@@ -121,9 +129,16 @@ export default function ListingDetailsPage() {
           <ListingDescription listing={listing} />
         </div>
 
+        {/* Right Side */}
         <div className="relative">
           <BookingCard listing={listing} />
         </div>
+      </div>
+
+      {/* Reviews Section */}
+      {/* receive review section */}
+      <div className="mt-16">
+        <ReviewSection listing={listing} refreshListing={refreshListing}/>
       </div>
     </div>
   );

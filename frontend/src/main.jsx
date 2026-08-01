5f5aa224953cf95abbcc1cf1110b3dbd0d4cff83
vivekkr620 <gk6629086@gmail.com>
2026-08-01 14:07:10 +0530

    Add Toast Container

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -22,12 +22,14 @@ import App from "./App";
 import "./index.css";
 
 import { AuthProvider } from "./context/AuthContext";
+import { ToastContainer } from"react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <App />
+        <ToastContainer />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
